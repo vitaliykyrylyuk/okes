1ee222dcb0e6502a9e0eccab74cf539c4d1b0abe
Vitalii Kyryliuk <vitalii_kyryliuk@epam.com>
2024-05-21 23:57:05 +0300

    Init okes app

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="container mx-auto">
+      <div class="max-w-sm mx-auto my-10 p-6 bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div class="card">
+          <div class="text-xl font-semibold">Your Name</div>
+          <p class="text-gray-500">Subtitle</p>
+          <button class="btn">Click me</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
