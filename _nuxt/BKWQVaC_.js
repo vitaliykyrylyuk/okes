e5938335285a5eeb3cb42fbfe57d26161661ce5b
Vitalii Kyryliuk <vitalii_kyryliuk@epam.com>
2025-01-23 20:36:46 +0200

    Updates

--- _nuxt/BKWQVaC_.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./DdX7jtEk.js";const o={};function r(t,a){return n(),c("div",null," Page: info ")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ _nuxt/CXBy5c1h.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./C2U-dkMe.js";const o={};function r(t,a){return n(),c("div",null," Page: info ")}const _=e(o,[["render",r]]);export{_ as default};
